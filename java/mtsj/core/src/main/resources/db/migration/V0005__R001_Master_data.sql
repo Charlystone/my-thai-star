a70dbbfa0645f9e64f9ef4c63ba50a9680dfb8f6
moritzAdietrich <1813050@stud.hs-mannheim.de>
2021-06-22 18:54:51 +0200

    db changes

--- java/mtsj/core/src/main/resources/db/migration/V0005__R001_Master_data.sql
+++ java/mtsj/core/src/main/resources/db/migration/V0005__R001_Master_data.sql
@@ -5,7 +5,7 @@ INSERT INTO UserRole(id, modificationCounter, name, active) VALUES (3, 1, 'Admin
 
 INSERT INTO User(id, modificationCounter, username, password, twoFactorStatus, email, idRole) VALUES (0, 1, 'Bob', '{bcrypt}$2a$10$qPM1WjcRKAffHxWXYEfPJOh2vGPlT/Fdv.hJX/LaZjzg/Wtj2csqO', false, 'bob@gmail.com', 0);
 INSERT INTO User(id, modificationCounter, username, password, twoFactorStatus, email, idRole) VALUES (1, 1, 'James', '{bcrypt}$2a$10$1CAKyUHbX6RJqT5cUP6/aOMTIlYYvGIO/a3Dt/erbYKKgmbgJMGsG', false, 'james@ctro4.de', 1);
-INSERT INTO User(id, modificationCounter, username, password, twoFactorStatus, email, idRole) VALUES (2, 1, 'Manager', '{bcrypt}$2a$10$IsTlZemkiPKE2gjtnSMlJOX5.uitNHXNRpLYyvyxNbHEhjpY.XdTq', false, 'manager@ctro4.de', 2);
+INSERT INTO User(id, modificationCounter, username, password, twoFactorStatus, email, idRole) VALUES (2, 1, 'Albert', '{bcrypt}$2a$10$IsTlZemkiPKE2gjtnSMlJOX5.uitNHXNRpLYyvyxNbHEhjpY.XdTq', false, 'manager@ctro4.de', 2);
 INSERT INTO User(id, modificationCounter, username, password, twoFactorStatus, email, idRole) VALUES (3, 1, 'Steve', '{bcrypt}$2a$10$qPM1WjcRKAffHxWXYEfPJOh2vGPlT/Fdv.hJX/LaZjzg/Wtj2csqO', false, 'admin@ctro4.de', 3);
 
 
